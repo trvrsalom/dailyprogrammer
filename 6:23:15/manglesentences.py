61ed7e5cca9abd5c17d267f33b6a82512d195c1e
Trevor Salom <trvrsalom@gmail.com>
2015-06-23 20:25:05 -0700

    Update manglesentences.py

--- 6:23:15/manglesentences.py
+++ 6:23:15/manglesentences.py
@@ -1,7 +1,7 @@
 import os
 import sys
 """
-	caveofprosperity.py
+	manglesentences.py
 	Created for: http://www.reddit.com/r/dailyprogrammer/comments/3aqvjn/20150622_challenge_220_easy_mangling_sentences/
 	Author: Trevor Salom
 """
