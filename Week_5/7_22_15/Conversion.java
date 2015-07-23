272008e96b8ca03bf33d4e4469ca6b2f050197fe
Trevor Salom <trvrsalom@gmail.com>
2015-07-22 22:42:16 -0700

    Whoops

--- Week_5/7_22_15/Conversion.java
+++ Week_5/7_22_15/Conversion.java
@@ -2,60 +2,62 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Scanner;
 
-
- public class Conversion {
-
-public static void main(String[] args) {
-
-    HashMap<String, Double> distRatios = new HashMap<String, Double>();
-    distRatios.put("inches", 39.3700787); //inch per m
-    distRatios.put("miles",.000621371); // mile per m
-    distRatios.put("attoparsecs", 32.4077929); //attparsecs per m
-    distRatios.put("meters", 1.0);//m per m
-
-    HashMap<String, Double> massRatios = new HashMap<String, Double>();
-     massRatios.put("kilograms", 1.0); //kg to kg
-     massRatios.put("pounds", 0.453592); //kg per oz
-     massRatios.put("ounces", 0.0283495); //kg per oz
-     massRatios.put("hhob", 440.7); //kg to hhob
+/**
+ * Shuffling.java
+ * Created for: https://www.reddit.com/r/dailyprogrammer/comments/2bxntq/7282014_challenge_173_easy_unit_calculator/
+ * Author: Trevor Salom
+ */
 
 
+public class Conversion {
 
-    while(true){
-    Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
 
-    System.out.println("Conversion (Use the format: {x} {units} to {units}): ");
+		HashMap < String, Double > distRatios = new HashMap < String, Double > ();
+		distRatios.put("inches", 39.3700787); //inch per m
+		distRatios.put("miles", .000621371); // mile per m
+		distRatios.put("attoparsecs", 32.4077929); //attparsecs per m
+		distRatios.put("meters", 1.0); //m per m
 
-    String[] userString = input.nextLine().split(" ");
+		HashMap < String, Double > massRatios = new HashMap < String, Double > ();
+		massRatios.put("kilograms", 1.0); //kg to kg
+		massRatios.put("pounds", 0.453592); //kg per oz
+		massRatios.put("ounces", 0.0283495); //kg per oz
+		massRatios.put("hhob", 440.7); //kg to hhob
 
-    String newUnit = userString[3];
-    String oldUnit = userString[1];
 
-    try{
 
-        if (distRatios.keySet().contains(userString[1])){
+		while (true) {
+			Scanner input = new Scanner(System. in );
 
-            Double value = distRatios.get(newUnit)/distRatios.get(oldUnit);
+			System.out.println("Conversion (Use the format: {x} {units} to {units}): ");
 
-            System.out.println(value * Integer.parseInt(userString[0]) + " " + userString[3]);
+			String[] userString = input.nextLine().split(" ");
 
-        }
+			String newUnit = userString[3];
+			String oldUnit = userString[1];
 
-        else{
-            Double value = massRatios.get(oldUnit)/massRatios.get(newUnit);
+			try {
 
-            System.out.println(value * Integer.parseInt(userString[0]) + " " + userString[3]);
-        }
+				if (distRatios.keySet().contains(userString[1])) {
 
-        }
-        catch(Exception e)
-        {
-            System.out.printf("Cannot convert %s %s to %s\n",
-                            userString[0], userString[1], userString[3]);
-        }
-    }
+					Double value = distRatios.get(newUnit) / distRatios.get(oldUnit);
 
+					System.out.println(value * Integer.parseInt(userString[0]) + " " + userString[3]);
+
+				} else {
+					Double value = massRatios.get(oldUnit) / massRatios.get(newUnit);
+
+					System.out.println(value * Integer.parseInt(userString[0]) + " " + userString[3]);
+				}
+
+			} catch (Exception e) {
+				System.out.printf("Cannot convert %s %s to %s\n",
+				userString[0], userString[1], userString[3]);
+			}
+		}
+
+
+	}
 
 }
-
-   }
