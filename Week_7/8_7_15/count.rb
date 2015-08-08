0c9a8f475743d01b6567c5ce438089f68caae6af
Trevor Salom <trvrsalom@gmail.com>
2015-08-07 20:12:10 -0700

    In response to: https://www.reddit.com/r/ProgrammingPrompts/comments/2sehb0/easy_letter_counter/

---
+++ Week_7/8_7_15/count.rb
@@ -0,0 +1,19 @@
+=begin
+ piglatin.rb
+ Created for: https://www.reddit.com/r/ProgrammingPrompts/comments/2sehb0/easy_letter_counter/
+ Author: Trevor Salom
+=end
+
+print "Input: "
+input = gets.chomp
+counts = Hash.new
+input.split("").each{ |letter|
+  if(counts.has_key?(letter))
+    counts[letter] += 1
+  else
+    counts[letter] = 1
+  end
+}
+counts.each do |key, value|
+  puts "#{key} - #{value}"
+end
